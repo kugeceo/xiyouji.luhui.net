4d204e3c7ebcde1f8452ba5aca63a39ceb9108cd
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2022-11-19 12:29:42 +0800

    Update header.js

--- header.js
+++ header.js
@@ -7,4 +7,4 @@ document.write(`<table border="2" cellSpacing="0" width="100%" bordercolor="#808
 </table>`)
 
 
-document.writeln("<script>var yreflocation_html=document.location.href;<\/script><script>var url=\"<iframe allowTransparency=true border=0 align=center vspace=0  hspace=0 name=searchbar marginwidth=0 marginheight=0 framespacing=0 frameborder=0 scrolling=no width=100% height=80\";url+=\'  src=\"https://www.luhui.net/search/bdun.html?url=\'+escape(yreflocation_html)+\'\"><\/iframe>\';document.write(url);<\/script>");
+document.writeln("<script>var yreflocation_html=document.location.href;<\/script><script>var url=\"<iframe allowTransparency=true border=0 align=center vspace=0  hspace=0 name=searchbar marginwidth=0 marginheight=0 framespacing=0 frameborder=0 scrolling=no width=100% height=80\";url+=\'  src=\"https://www.luhui.net/search/bdun.html#?url=\'+escape(yreflocation_html)+\'\"><\/iframe>\';document.write(url);<\/script>");
