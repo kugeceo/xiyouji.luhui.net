c3ac173aac40fb5378c8b4c1dce843ec64feeafd
鲁虺 <37980424+kugeceo@users.noreply.github.com>
2022-11-19 11:22:49 +0800

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write(`<script src="/static/js/css3-mediaqueries.js"></script>`)
+
+document.write(`<script src="/static/js/css3-mediaqueries.min.js"></script>`)
+
+document.write(`Powered by
+2004-01-11  <a href="http://www.luhui.net" target="_blank">luhui.net</a> All Rights Reserved.<footer>`)
---
+++ header.js
@@ -0,0 +1,10 @@
+document.write(`<table border="2" cellSpacing="0" width="100%" bordercolor="#808080">
+  <tr>
+    <td align="middle" bgColor="#ffff00" borderColorLight="#000000" width="73">
+      <p align="center"><script src="http://www.luhui.net/search/js/link.js"></script></p>
+    </td>
+  </tr>
+</table>`)
+
+
+document.writeln("<script>var yreflocation_html=document.location.href;<\/script><script>var url=\"<iframe allowTransparency=true border=0 align=center vspace=0  hspace=0 name=searchbar marginwidth=0 marginheight=0 framespacing=0 frameborder=0 scrolling=no width=100% height=80\";url+=\'  src=\"http://www.luhui.net/search/bdun.html?url=\'+escape(yreflocation_html)+\'\"><\/iframe>\';document.write(url);<\/script>");
